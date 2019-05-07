813c59252de58aa6cf05e73205b1155494f2746f
astadipati <astadipati@outlook.com>
2019-05-07 10:37:05 +0700

    Detil All

--- lib/detail.dart
+++ lib/detail.dart
@@ -13,6 +13,21 @@ class _DetailState extends State<Detail> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("${widget.list[widget.index]['item_name']}"),),
+      body: new Container(
+        padding: const EdgeInsets.all(20.0),
+        child: new Card(
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text(widget.list[widget.index]['item_name'], style: new TextStyle(fontSize: 20.0),),
+                new Text("Code : ${widget.list[widget.index]['item_code']}", style: new TextStyle(fontSize: 18.0),),
+                new Text("Harga : ${widget.list[widget.index]['harga']}", style: new TextStyle(fontSize: 18.0),),
+                new Text("Stok : ${widget.list[widget.index]['stock']}", style: new TextStyle(fontSize: 18.0),)
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
