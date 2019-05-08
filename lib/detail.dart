5980d19b12e7b95905b75d8a2f4794ec1f4fb776
astadipati <astadipati@outlook.com>
2019-05-08 07:20:18 +0700

    Deletedata

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'editdata.dart';
+import 'package:http/http.dart' as http;
+import 'main.dart';
 
 class Detail extends StatefulWidget {
   List list;
@@ -10,6 +12,58 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
+  void deleteData(){
+ var url = "http://192.168.2.203/api_flutter/deletedata.php";
+ 
+  http.post(url, body: {
+      'id' : widget.list[widget.index]['id'],
+  });
+  }
+  void confirm(){
+// coba cara lain
+  var alert = new AlertDialog(
+    title: new Text("Hapus Data '${widget.list[widget.index]['item_name']}'"),
+    actions: <Widget>[
+      new RaisedButton(
+        child: new Text("Hapus"),
+        color: Colors.red,
+        onPressed: (){
+          deleteData();
+          new MaterialPageRoute(
+            builder: (BuildContext context)=>new MyApp(),
+            // Navigator.pop(_context);
+          );
+        },
+      ),
+      new RaisedButton(
+        child: new Text("Cancel", style: TextStyle(color: Colors.black),),
+        onPressed: ()=>Navigator.pop(context),
+      )
+    ],
+  );
+  showDialog(context: context, builder: (_)=>alert);
+// end cara lain
+
+    // AlertDialog alertDialog = new AlertDialog(
+    //   content: new Text("Hapus Data '${widget.list[widget.index]['item_name']}'"),
+    //   actions: <Widget>[
+    //     new RaisedButton(
+    //       child: new Text("Hapus", style: new TextStyle(color: Colors.black)),
+    //       color: Colors.red,
+    //       onPressed: (){},
+    //     ),
+    //     new RaisedButton(
+    //       child: new Text("Cancel", style: new TextStyle(color: Colors.black)),
+    //       color: Colors.blue,
+    //       onPressed: ()=>Navigator.pop(context),
+    //     )
+    //   ],
+    // );
+    // showDialog(context: context, child: alertDialog);
+    // showDialog(context: context,builder: (_)=>alertDialog);
+    // builder: ()=>new Text("Hello"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -43,7 +97,7 @@ class _DetailState extends State<Detail> {
                   new RaisedButton(
                     child: new Text("Hapus"),
                     color: Colors.red,
-                    onPressed: (){},
+                    onPressed: ()=>confirm(),
                   )
                 ],)
               ],
